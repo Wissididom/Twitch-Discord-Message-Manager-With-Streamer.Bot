587654ea42a21309e13215dc29163d9e374e3886
Wissididom <webmaster@wissididom.me>
2023-07-31 10:15:18 +0200

    Ran prettier

--- getActions.js
+++ getActions.js
@@ -1,29 +1,36 @@
-import * as DotEnv from 'dotenv';
+import * as DotEnv from "dotenv";
 
 DotEnv.config();
 
 function sanitizeUrl(url) {
-    if (!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = 'http://' + url;
-    }
-    url = url.replace('///', '//');
-    if (!url.endsWith('/')) {
-        url += '/';
-    }
-    return url;
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    url = "http://" + url;
+  }
+  url = url.replace("///", "//");
+  if (!url.endsWith("/")) {
+    url += "/";
+  }
+  return url;
 }
 
 async function getActions() {
-    let sanitizedUrl = sanitizeUrl(process.env['STREAMER_BOT_HTTP_SERVER']) + 'GetActions';
-    let response = await fetch(sanitizedUrl);
-    if (response.status == 200) {
-        let actions = (await response.json()).actions;
-        for (let i = 0; i < actions.length; i++) {
-            console.log(`${actions[i].id}: ${actions[i].name} (${actions[i].enabled ? 'Enabled' : 'Disabled'})`);
-        }
-    } else {
-        return new Promise.reject('Non 200 response from Streamer.Bot\'s HTTP Server!')
+  let sanitizedUrl =
+    sanitizeUrl(process.env["STREAMER_BOT_HTTP_SERVER"]) + "GetActions";
+  let response = await fetch(sanitizedUrl);
+  if (response.status == 200) {
+    let actions = (await response.json()).actions;
+    for (let i = 0; i < actions.length; i++) {
+      console.log(
+        `${actions[i].id}: ${actions[i].name} (${
+          actions[i].enabled ? "Enabled" : "Disabled"
+        })`,
+      );
     }
+  } else {
+    return new Promise.reject(
+      "Non 200 response from Streamer.Bot's HTTP Server!",
+    );
+  }
 }
 
 getActions();
