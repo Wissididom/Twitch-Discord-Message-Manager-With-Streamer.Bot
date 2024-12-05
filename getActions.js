b4ffb25cae55b6b25425afab9d4f402493041bc6
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-12-05 20:36:47 +0100

    Remove http server necessaty and the getActions function from index.js

--- getActions.js
+++ getActions.js
@@ -1,34 +1,32 @@
-import "dotenv/config";
-
-function sanitizeUrl(url) {
-  if (!url.startsWith("http://") && !url.startsWith("https://")) {
-    url = "http://" + url;
-  }
-  url = url.replace("///", "//");
-  if (!url.endsWith("/")) {
-    url += "/";
-  }
-  return url;
-}
+import { StreamerbotClient } from "@streamerbot/client";
 
 async function getActions() {
-  let sanitizedUrl = sanitizeUrl(process.env["STREAMER_BOT_HTTP_SERVER"]) +
-    "GetActions";
-  let response = await fetch(sanitizedUrl);
-  if (response.status == 200) {
-    let actions = (await response.json()).actions;
-    for (let i = 0; i < actions.length; i++) {
-      console.log(
-        `${actions[i].id}: ${actions[i].name} (${
-          actions[i].enabled ? "Enabled" : "Disabled"
-        })`,
-      );
-    }
-  } else {
-    return new Promise.reject(
-      "Non 200 response from Streamer.Bot's HTTP Server!",
-    );
-  }
+  return await new Promise((resolve, reject) => {
+    const SBClient = new StreamerbotClient({
+      host: Deno.env.get("STREAMER_BOT_WS_SERVER_HOST"),
+      port: Deno.env.get("STREAMER_BOT_WS_SERVER_PORT"),
+      onConnect: async (_data) => {
+        //console.log("Streamer.Bot Connection opened!");
+        resolve((await SBClient.getActions()).actions);
+        await SBClient.disconnect();
+      },
+      onDisconnect: () => {
+        //console.log("Streamer.Bot Connection closed!");
+        reject();
+      },
+      onError: (err) => {
+        //console.log("Streamer.Bot Connection errored!", err);
+        reject(err);
+      },
+    });
+  });
 }
 
-getActions();
+const actions = await getActions();
+for (let i = 0; i < actions.length; i++) {
+  console.log(
+    `${actions[i].id}: ${actions[i].name} (${
+      actions[i].enabled ? "Enabled" : "Disabled"
+    })`,
+  );
+}
